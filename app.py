c2eefd266be9672ce8675ab4670ef4366fcbe106
anilbey <tuncel.manil@gmail.com>
2017-11-11 18:13:48 +0100

    serve static files

--- app.py
+++ app.py
@@ -1,34 +1,38 @@
 import psycopg2
 from flask import Flask
+from flask import request, send_from_directory
 import json
-app = Flask(__name__)
+app = Flask(__name__ , static_url_path='')
 
 
-
+@app.route('/')
+def root():
+	return "obama"
 
 @app.route('/atms')
 def atms():
 	try:
 		conn = connect()
-		# create a psycopg2 cursor that can execute queries
 		cursor = conn.cursor()
-		# create a new table with a single column called "name"
-		#cursor.execute("""CREATE TABLE tutorials (name char(40));""")
-		# run a SELECT statement - no data in there, but we can try it
-		cursor.execute("""SELECT * from atms""")
+		cursor.execute("SELECT * from atms")
 		rows = cursor.fetchall()
 		return json.dumps(rows)
 	except Exception as e:
-		print("Uh oh, can't connect. Invalid dbname, user or password?")
 		print(e)
 	return e
 
+# to serve static files
+@app.route('/sample_data/<path:path>')
+def serve_file(path):
+    return send_from_directory('sample_data', path)
 
 def connect():
 	connect_str = "dbname='lauzhack' user='lauzhack' host='165.227.134.175' password='lauzhack2017'"
 	# use our connection values to establish a connection
 	conn = psycopg2.connect(connect_str)
 	return conn
+
+
 
 
 if __name__ == '__main__':
