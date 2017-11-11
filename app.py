fdbf9f82599934a7bbdd6d041e11cf63c49b5580
anilbey <tuncel.manil@gmail.com>
2017-11-11 22:44:10 +0100

    example_notebook

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import psycopg2
 from flask import Flask
 from flask import request, send_from_directory
 import json
+from neo4j.v1 import GraphDatabase, basic_auth
 app = Flask(__name__ , static_url_path='')
 
 
@@ -22,15 +23,19 @@ def atms():
 	return e
 
 # to serve static files
-@app.route('/sample_data/<path:path>')
+@app.route('/static/<path:path>')
 def serve_file(path):
-    return send_from_directory('sample_data', path)
+    return send_from_directory('static', path)
 
 def connect():
 	connect_str = "dbname='lauzhack' user='lauzhack' host='165.227.134.175' password='lauzhack2017'"
 	# use our connection values to establish a connection
 	conn = psycopg2.connect(connect_str)
 	return conn
+
+def ne04j_new_session():
+	driver = GraphDatabase.driver("bolt://165.227.134.175", auth=basic_auth("user", "lauzhack2017"))
+	return driver.session()
 
 
 
